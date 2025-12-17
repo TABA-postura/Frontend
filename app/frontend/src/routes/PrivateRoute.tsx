95cecb60752339a2ce5ebcab3013bb769123f630
doyeon112 <cherry3832@naver.com>
2025-12-17 17:10:36 +0900

    fix: restore authentication check in PrivateRoute

--- app/frontend/src/routes/PrivateRoute.tsx
+++ app/frontend/src/routes/PrivateRoute.tsx
@@ -1,11 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoute() {
-  // TODO: 개발 완료 후 아래 주석 해제하고 return <Outlet /> 삭제
-  // const accessToken = localStorage.getItem("accessToken");
-  // if (!accessToken) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
 
   return <Outlet />;
 }
